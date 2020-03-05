36700160cada5481c4a32b349bac954d256381ac
Dee Sharpe <demetrioussharpe@aim.com>
2020-03-05 14:41:52 -0500

    Just added code

---
+++ OldTrip/src/test/java/com/apollo/test/MyTest.java
@@ -0,0 +1,11 @@
+package com.apollo.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+public class MyTest {
+	@Test
+	public void test() {
+		assertEquals("True is true", true, true);
+	}
+}
